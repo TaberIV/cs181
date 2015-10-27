2fa24c2a6d4d8c721f2f7bb6ca5b745ec72d97ea
Taber McFarlin <tabermcfarlin@gmail.com>
2015-10-27 16:32:14 -0400

    This is where I started the Guitar Hero class

--- java/prgmAssign/assign3/RingBuffer.java
+++ java/prgmAssign/assign3/RingBuffer.java
@@ -1,3 +1,65 @@
+/*
+ * RingBuffer.java
+ *
+ * "I pledge my honor I have abided by the Stevens Honor System."
+ *
+ * Taber McFarlin (c) 2015
+ *
+ */
+
+package assign3;
+
 /**
- * Implemants code for ring buffer feedback mechanism from Karplus-Strong algorithm
+ * Implemants code for ring buffer feedback  
+ * mechanism from Karplus-Strong algorithm
  * 
+ * @author Taber McFarlin
+ * @since 20151027
+ *
+ */
+public class RingBuffer {
+    private double[] buffer;
+    private int first, last;
+    
+    public RingBuffer(int capacity) {
+	buffer = new double[capacity];
+	first = 0;
+	last = -1;
+    }
+
+    int getSize() {
+	return Math.abs(first - (last + 1));
+    }
+
+    boolean isEmpty() {
+	return (first == last);
+    }
+    
+    boolean isFull() {
+	return (((last + 1) % buffer.length) == first);
+    }
+    
+    void enqueue(double x) {
+	if(!isFull()) {
+	    last = (last + 1) % buffer.length;
+	}
+	else {
+	    //Throw some error
+	}
+	buffer[last] = x;
+    }
+    
+    double peek() {
+	return buffer[first];
+    }
+    
+    double dequeue() {
+	if(!isEmpty()) {
+	    first = (first + 1) % buffer.length;
+	}
+	else {
+	    //Throw some error
+	}
+	return buffer[first - 1];
+    }
+}
